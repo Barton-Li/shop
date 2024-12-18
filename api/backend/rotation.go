513496da2b174f8ccf17efb6ac122bc83b7c6e37
Barton-Li <2839085178@qq.com>
2024-12-18 17:27:23 +0800

    软删除

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -12,3 +12,9 @@ type RotationReq struct {
 type RotationRes struct {
 	RotationId int `json:"rotation_id"`
 }
+
+type RotationDeleteReq struct {
+	g.Meta `path:"/rotation/delete" method:"delete" tags:"轮播图" summary:"删除轮播图接口"`
+	Id     uint `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
+}
+type RotationDeleteRes struct{}
